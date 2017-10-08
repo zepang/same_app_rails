e5bd48c684b01772d73f5f70c9d47abea3a279c0
uncleze2017 <ummlq4@gmail.com>
2017-10-08 22:34:21 +0800

    完成登录和记住我的功能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination' # 不完全正确
-      render 'new'
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
+
   def new
     @user = User.new
   end
-
   def show
     @user = User.find(params[:id])
   end
-
   def create
     @user = User.new(user_params) # 不是最终的实现方式
     if @user.save
@@ -17,11 +18,29 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 end
